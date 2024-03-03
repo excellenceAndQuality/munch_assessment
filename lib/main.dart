05975337344ab19b128104d1ff24b0a6d388bca0
Roy Langa Jr <roy@Roys-MacBook-Pro.local>
2024-03-04 00:51:25 +0200

    moved main from ui for apk building purposes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:roy_munch_assessment/styles/colours.dart';
-import '../blocs/login/login_bloc.dart';
-import 'pages/landing_page.dart';
+import 'blocs/login/login_bloc.dart';
+import 'ui/pages/landing_page.dart';
 
 void main() {
   runApp(const MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:roy_munch_assessment/ui/main.dart';
+import 'package:roy_munch_assessment/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
